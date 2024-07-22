9ffee23afafbda86ea48ac15fc34203b1f04cc28
Vivek Jha <vivek.x.jha@gmail.com>
2024-07-21 22:40:39 -0700

    Added truncated_dir plugin and cleaed up non-used ones from directory

---
+++ status/truncated_dir.sh
@@ -0,0 +1,41 @@
+show_truncated_dir() {
+  # Shortens given path from '/this/is/an/example/super/long/actually/really/long/path' -> 't/i/a/e/s/l/a/r/l/path'
+  shorten_path() {
+    local path="$1"
+    # local char_limit="$2"
+    local shortened_path=""
+
+    # Check if the path length is greater than 30
+    if [ ${#path} -gt 20 ]; then
+      # Split the path by '/'
+      IFS='/' read -ra parts <<< "$path"
+
+      # Store the last part of the path
+      local last_part="${parts[${#parts[@]}-1]}"
+
+      # Loop through the parts except the last one
+      for ((i=0; i < ${#parts[@]} - 1; i++)); do
+        if [ -n "${parts[$i]}" ]; then
+          # Append the first character of each part
+          shortened_path+="/${parts[$i]:0:1}"
+        fi
+      done
+      # Add the last part of the path (filename or last directory)
+      shortened_path+="/${last_part}"
+  else
+      shortened_path="$path"
+    fi
+
+    echo "$shortened_path"
+  }
+
+  local index=$1 # This variable is used internally by the module loader in order to know the position of this module
+  local icon="$( get_tmux_option "@catppuccin_truncated_dir_icon"  " ")"
+  local color="$(get_tmux_option "@catppuccin_truncated_dir_color" "default")"
+  local text="$( get_tmux_option "@catppuccin_truncated_dir_text"  "#{pane_current_path}")"
+
+  module=$(build_status_module "$index" "$icon" "$color" "$text")
+
+  echo "$module"
+}
+
